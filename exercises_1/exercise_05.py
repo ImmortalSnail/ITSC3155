ebd8d10ebcf6a5116d869fd24fffe6f76be34e02
Ronnieh428 <teddybrosevelt8@gmail.com>
2023-01-12 18:07:47 -0500

    exercise 5 done

---
+++ exercises_1/exercise_05.py
@@ -0,0 +1,20 @@
+# Lists
+list = []
+list2 = []
+result_list = []
+
+# Populate lists
+for x in range(5):
+    list.append(int(input("Enter a number for list 1: ")))
+for x in range(5):
+    list2.append(int(input("Enter a number for list 2: ")))
+
+# Generates results
+for x in list:
+    var = list[x - 1]
+    for x in list2:
+        if var == list2[x - 2] and result_list.count(var) == 0:
+            result_list.append(var)
+
+# Prints finished lists
+print(str(list) + "\n" + str(list2) + "\n" + str(result_list))
