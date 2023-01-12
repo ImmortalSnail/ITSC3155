6ce55eb2cdd9e8085fd305ec7df3d97bad67efb6
Ronnieh428 <teddybrosevelt8@gmail.com>
2023-01-12 16:14:44 -0500

    updated files, completed 4

---
+++ exercise_04.py
@@ -0,0 +1,10 @@
+# Variables
+list_size = int(input("Enter list size: "))
+list = []
+
+# Adds to list
+for x in range(list_size):
+    list.append(float(input("Please enter a float: ")))
+
+#Prints result
+print(list)
