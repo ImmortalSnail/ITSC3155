323ef62ec786e499e1d2d97f7d47add3c3d29a64
Ronnieh428 <teddybrosevelt8@gmail.com>
2023-01-12 15:27:28 -0500

    finished exercise 2

--- exercises_1/exercise_02.py
+++ exercises_1/exercise_02.py
@@ -1,3 +1,12 @@
 word1 = input("Enter a string: ")
 word2 = input("Enter a different string: ")
 
+result1 = word1.endswith(word2)
+result2 = word2.endswith(word1)
+
+if result1:
+    print(result1)
+elif result2:
+    print(result2)
+else:
+    print(False)
