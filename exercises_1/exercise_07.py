90ea45ab24e162af8bf2c49bdb05c2a74c16ce8e
Ronnieh428 <teddybrosevelt8@gmail.com>
2023-01-12 19:52:41 -0500

    exercise 7 comments added

--- exercises_1/exercise_07.py
+++ exercises_1/exercise_07.py
@@ -1,7 +1,9 @@
+# Variables
 quit = True
 list = []
 even_list = []
 
+# Determines what should occur based on user input
 while quit:
     var = input("Please enter an integer or type QUIT to stop: ")
     if var == "QUIT":
@@ -12,5 +14,6 @@ while quit:
     else: 
         list.append(int(var))
 
+# Outputs results after QUIT
 print("Numbers entered: " + str(list))
 print("Even numbers: " + str(even_list))
