51c64bf9304c069dceab2a2093deae7097bc9414
Ronnieh428 <teddybrosevelt8@gmail.com>
2023-01-12 21:14:37 -0500

    All exercises finished

---
+++ exercises_1/exercise_09.py
@@ -0,0 +1,11 @@
+# Variables
+list = []
+
+# Gets words from users
+for x in range(5):
+    list.append(input("Enter a word: "))
+
+# Outputs
+print("Words: " + str(list))
+print("One string:", end = " ")
+print(" ".join(list))
